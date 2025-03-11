1d01277b8963e9c1c43f0bae9aa2478bad7fdab8
zoebailey <113046758+zoebailey@users.noreply.github.com>
2025-03-11 16:05:29 -0400

    Add files via upload

--- top-cities.js
+++ top-cities.js
@@ -6,9 +6,11 @@ const walkability = [6, 54, 3, 21, 7];
 const temps = [8, 9, 90, 78, 56];
 const precipitation = [43, 12, 0, 98, 67];
 
+// update results when a new selection is made
 function updateResults(selectedValue) {
     let sortedArray = [];
 
+    // sorted high to low except for "coldest" sorted low to high
     if (selectedValue == "overall") {
         sortedArray = overall.sort((a, b) => b - a);
     } else if (selectedValue == "walkable") {
@@ -21,12 +23,17 @@ function updateResults(selectedValue) {
         sortedArray = precipitation.sort((a, b) => b - a);
     }
 
+    // display in console for debugging
     console.log(sortedArray);
+
+    // display results to user
     resultsDiv.innerHTML = `<p>Results:<br> ${sortedArray.join("<br>")}</p>`;
 }
 
+// if no attribute is selected, auto select overall
 updateResults("overall");
 
+// when attribute is changed, call updateResults()
 selectElement.addEventListener("change", function() {
     updateResults(selectElement.value);
 
