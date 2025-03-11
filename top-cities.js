b452f6dc7351de3498a353f104f3fdea7bdc4768
zoebailey <113046758+zoebailey@users.noreply.github.com>
2025-03-11 12:33:27 -0400

    Add files via upload

--- top-cities.js
+++ top-cities.js
@@ -1,3 +1,33 @@
 const selectElement = document.getElementById("attribute");
-const selectedValue = selectElement.value;
-console.log(selectedValue);
+const resultsDiv = document.getElementById("results");
+
+const overall = [1, 2, 3, 4, 5]; // will be replaced with API call
+const walkability = [6, 54, 3, 21, 7];
+const temps = [8, 9, 90, 78, 56];
+const precipitation = [43, 12, 0, 98, 67];
+
+function updateResults(selectedValue) {
+    let sortedArray = [];
+
+    if (selectedValue == "overall") {
+        sortedArray = overall.sort((a, b) => b - a);
+    } else if (selectedValue == "walkable") {
+        sortedArray = walkability.sort((a, b) => b - a);
+    } else if (selectedValue == "warmest") {
+        sortedArray = temps.sort((a, b) => b - a);
+    } else if (selectedValue == "coldest") {
+        sortedArray = temps.sort((a, b) => a - b);
+    } else if (selectedValue == "rainiest") {
+        sortedArray = precipitation.sort((a, b) => b - a);
+    }
+
+    console.log(sortedArray);
+    resultsDiv.innerHTML = `<p>Results:<br> ${sortedArray.join("<br>")}</p>`;
+}
+
+updateResults("overall");
+
+selectElement.addEventListener("change", function() {
+    updateResults(selectElement.value);
+
+});
